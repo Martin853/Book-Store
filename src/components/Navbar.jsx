e8411f3470ad25539cec07d60f9c5bad5bce6db2
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-25 22:05:23 +0200

    Added icons to the navbar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { BsBook } from 'react-icons/bs';
 
 export const Navbar = () => {
   return (
@@ -16,15 +18,17 @@ export const Navbar = () => {
       </div>
       <div className="flex w-3/4 justify-center gap-4 md:flex-1 md:justify-end">
         <button
-          className="font-poppins font-semibold w-2/5 p-1 text-sm bg-transparent hover:bg-hoverGreen
-        ] border border-black rounded-md cart-button "
+          className="font-poppins font-semibold w-1/2 p-1 text-sm bg-transparent hover:bg-hoverGreen
+        ] border border-black rounded-md cart-button flex justify-center items-center gap-2 md:w-2/5 "
         >
+          <BsBook size={'20px'} />
           Books
         </button>
         <button
-          className="font-poppins font-semibold w-2/5 p-1 text-sm bg-transparent hover:bg-hoverGreen
-        ] border border-black rounded-md cart-button "
+          className="font-poppins font-semibold w-1/2 p-1 text-sm bg-transparent hover:bg-hoverGreen
+        ] border border-black rounded-md cart-button flex justify-center items-center gap-2 md:w-2/5"
         >
+          <AiOutlineShoppingCart size={'20px'} />
           Cart
         </button>
       </div>
