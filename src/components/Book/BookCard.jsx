35086b4df3190f8ea6fa24a868c9a5812840b972
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-26 22:37:00 +0200

    Removed the underline from the title

--- src/components/Book/BookCard.jsx
+++ src/components/Book/BookCard.jsx
@@ -24,7 +24,7 @@ export const BookCard = () => {
           src="https://edit.org/images/cat/book-covers-big-2019101610.jpg"
           className=" w-1/2 h-3/4"
         />
-        <h1 className="font-poppins font-bold text-2xl underline">Book Name</h1>
+        <h1 className="font-poppins font-bold text-2xl ">Book Name</h1>
       </div>
 
       {modal && <BookCardModal toggleModal={toggleModal} />}
