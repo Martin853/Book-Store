a0055e52ba11e39f95f58229c6240fa7235699aa
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-28 00:11:02 +0200

    Created empty card functionality

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { CartItem } from '../components/Cart/CartItem';
+import { Link } from 'react-router-dom';
 
 export const Cart = () => {
   const { cart } = useSelector((state) => state.shoppingCart);
-  console.log(cart);
+
+  if (cart.length === 0) {
+    return (
+      <div className="w-full h-full flex flex-col gap-4 justify-center items-center p-4 mx-auto">
+        <h1 className="font-poppins text-center text-2xl font-bold">
+          You don't have anything in your cart
+        </h1>
+        <Link
+          className="font-poppins font-semibold p-2 text-base bg-transparent hover:bg-lime-600
+        ] border border-black rounded-lg cart-button flex justify-center items-center gap-2  transition-all duration-100 ease-linear"
+          to={'/'}
+        >
+          Go back shopping
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className=" mt-2 p-2 mx-auto h-full flex flex-col gap-4 ">
       {cart.map((item) => (
