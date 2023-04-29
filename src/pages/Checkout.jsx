399e3dfc505fb77299ef66050c055d8f0b90b6c9
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-29 22:24:04 +0200

    Removed unneccessary code

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 export const Checkout = () => {
   const { cart } = useSelector((state) => state.shoppingCart);
